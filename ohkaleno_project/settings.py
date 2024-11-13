e3d1cf8152088d163d0100c33e04dd2a2d83b1d3
lana-z <lana@levelupeconomy.com>
2024-11-12 20:39:01 -0800

    remove protocol (https://) from allowed hosts

--- ohkaleno_project/settings.py
+++ ohkaleno_project/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['ohkaleno.netlify.app', 'https://oh-kale-no-backend.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ohkaleno.netlify.app', 'oh-kale-no-backend.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
